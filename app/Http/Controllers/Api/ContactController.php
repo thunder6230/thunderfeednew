a7e8bf48e7279bdeedb9e39be76249e57841852a
thunder6230 <62564512+thunder6230@users.noreply.github.com>
2021-04-15 15:20:47 +0200

    working pusher chat messages

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -6,21 +6,24 @@ use App\Models\User;
 use App\Models\Friend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ContactController extends Controller
 {
-    public function get(User $user){
+    public function get(){
         // $contacts = $user->friends->friend_id;
         $contacts = [];
-        foreach($user->friends as $friend){
+        foreach(Auth::user()->friends as $friend){
             $user = User::find($friend->friend_id);
             $contacts[] = $user;
         }
+
+
         return response()->json($contacts);
     }
     public function getAll(){
         
-        return response()->json(User::all());
+        return response()->json(User::all()->where('id', '!=', Auth::user()->id));
     }
     
 }
